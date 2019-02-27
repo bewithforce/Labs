c1a7f6161bf4111207eadb30e480f737ea538ddc
Dzianis <somniummisellus@gmail.com>
2019-02-27 17:49:58 +0300

    first and second labs

---
+++ src/Lab2.kt
@@ -0,0 +1,35 @@
+import java.util.*
+
+fun main(args : Array<String>) {
+    val input = Scanner(System.`in`)
+    val n = input.nextInt()
+    val array = Array(n, { IntArray(n) })
+    var count = 0
+    for (i in array.indices) {
+        for (j in array[i].indices) {
+            array[i][j] = (-100..100).random()
+            print("%4d".format(array[i][j]))
+        }
+        println()
+    }
+
+    loop@
+    for (i in array.indices) {
+        var min = array[i][0]
+        var minIndex = 0
+        for (j in array[i].indices) {
+            if (array[i][j] < min) {
+                min = array[i][j]
+                minIndex = j
+            }
+        }
+        var max = array[i][minIndex]
+        for (j in array[i].indices) {
+            if (array[j][minIndex] > max) {
+                continue@loop
+            }
+        }
+        count++
+    }
+    println(count)
+}
